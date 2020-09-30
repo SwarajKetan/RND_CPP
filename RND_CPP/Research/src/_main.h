45eb7cd29f05ed462824792e5bc5f929a1f7ee12
Swaraj <swarajksantra@gmail.com>
2020-09-30 23:13:37 +0530

    adding more headers

--- RND_CPP/Research/src/_main.h
+++ RND_CPP/Research/src/_main.h
@@ -2,4 +2,11 @@
 #include <memory>
 #include <typeinfo>
 #include <exception>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <future>
+#include <queue>
+#include <stack>
+#include <memory>
 #define RUN(runnable) int main(){ try{ std::cout << "Running.." << typeid(runnable).name() << std::endl;{std::make_unique<runnable>()->Run();} std::getchar();} catch(const std::exception ex){std::cout << "exception: " << ex.what() <<std::endl;}}
